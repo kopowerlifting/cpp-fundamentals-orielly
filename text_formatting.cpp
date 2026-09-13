034660190876ca7874eeae411bbda7033897b355
kopowerlifting <odonnell.kjgr@gmail.com>
2026-09-13 14:58:43 +1000

    feat: sum with for loop and output with text formatting

---
+++ text_formatting.cpp
@@ -0,0 +1,16 @@
+// text_formatting.cpp
+// Summing integers with the for statement; introducing text formatting
+#include <format>
+#include <iostream>
+using namespace std;
+
+int main() {
+    int total{0};
+
+    // total even intergers from 2 through 20
+    for (int number{2}; number <= 20; number +=2) {
+        total += number;
+    }
+
+    cout << format("Sum is {}\n", total);
+}
