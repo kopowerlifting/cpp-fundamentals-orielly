70efce6175e451ae5104cc0a310b85abfe19a02c
kopowerlifting <odonnell.kjgr@gmail.com>
2026-09-13 14:53:16 +1000

    feat: more for loop counter examples

--- for_counter_controlled_iteration.cpp
+++ for_counter_controlled_iteration.cpp
@@ -10,5 +10,42 @@ int main() {
         cout << counter << " ";
     }
 
+    // additional examples
+    cout << "\n\nVary control variable from 1 to 100 in increments of 1:\n";
+    
+    for (int i{1}; i <= 100; ++i) {
+        cout << i << " ";
+    }
+
+    cout << "\n\nVary control variable from 100 down to 1 in decrements of 1:\n";
+
+    for (int i{100}; i >= 1; --i) {
+        cout << i << " ";
+    }
+
+    cout << "\n\nVary control variable from 7 to 77 in increments of 7:\n";
+
+    for (int i{7}; i <= 77; i += 7) {
+        cout << i << " ";
+    }
+
+    cout << "\n\nVary control variable from 20 down to 2 in decrements of 2:\n";
+
+    for (int i{20}; i >= 2; i -= 2) {
+        cout << i << " ";
+    }
+
+    cout << "\n\nVary control variable over the values 2, 5, 6, 11, 14, 17, 20\n";
+
+    for (int i{2}; i <= 20; i += 3) {
+        cout << i << " ";
+    }
+
+    cout << "\n\nVary control variable over the values 99, 88, 77 ... 0\n";
+
+    for (int i{99}; i >= 0; i -= 11) {
+        cout << i << " ";
+    }
+
     cout << "\n";
 }
